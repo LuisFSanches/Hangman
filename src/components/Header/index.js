2680ef3f3d2a2551d5e9f2f34b58ca1115ff574b
S4nches <luis-lipe@hotmail.com>
2020-08-08 15:53:08 -0300

    Hangman project

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Header() {
+  return(
+    <>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter</p>
+    </>
+  )
+}
+
+export default Header;
